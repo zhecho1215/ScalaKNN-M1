027a85b619dfc64d81186a1c4d03bfc35759ea27
Arina Raileanu <raileanu.arina1@gmail.com>
2022-03-03 10:27:07 +0100

    B3 timings

--- cs449-Template-M1-master/src/main/scala/predict/Baseline.scala
+++ cs449-Template-M1-master/src/main/scala/predict/Baseline.scala
@@ -1,22 +1,16 @@
 package predict
 
-import org.rogach.scallop._
-import org.apache.spark.rdd.RDD
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
-
-import scala.math
+import org.rogach.scallop._
 import shared.predictions._
-
-import scala.math.abs
 
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
   val train = opt[String](required = true)
   val test = opt[String](required = true)
-  val separator = opt[String](default=Some("\t"))
-  val num_measurements = opt[Int](default=Some(0))
+  val separator = opt[String](default = Some("\t"))
+  val num_measurements = opt[Int](default = Some(0))
   val json = opt[String]()
   verify()
 }
@@ -28,47 +22,47 @@ object Baseline extends App {
   val spark = SparkSession.builder()
     .master("local[1]")
     .getOrCreate()
-  spark.sparkContext.setLogLevel("ERROR") 
+  spark.sparkContext.setLogLevel("ERROR")
 
   println("")
   println("******************************************************")
 
-  var conf = new Conf(args) 
+  var conf = new Conf(args)
   // For these questions, data is collected in a scala Array 
   // to not depend on Spark
-  println("Loading training data from: " + conf.train()) 
+  println("Loading training data from: " + conf.train())
   val train = load(spark, conf.train(), conf.separator()).collect()
-  print(train(1))
-  println("Loading test data from: " + conf.test()) 
+  println("Loading test data from: " + conf.test())
   val test = load(spark, conf.test(), conf.separator()).collect()
 
-  // Apply preprocessing operations on the train and test
-  val normalizedTrain = preprocess(train)
-  val normalizedTest = preprocess(test)
+  // Initialize the solvers for questions in B
+  val solvers = new BSolvers(train, test)
 
-  // Average rating statistics
-  val meanGlobal = mean(train.map(x => x.rating))
-  val user1Avg = mean(train.filter(x => x.user == 1).map(x => x.rating))
-  val item1Avg = mean(train.filter(x => x.item == 1).map(x => x.rating))
-  val item1AvgDev = std(train.filter(x => x.item == 1).map(x => x.rating))
-  val item1AvgDevScale = stdScale(train.filter(x => x.item == 1).map(x => x.rating))
-  val predUser1Item1 = user1Avg + item1AvgDev * scaleUserRating(user1Avg + item1AvgDev, user1Avg)
-
-  val measurements = (1 to conf.num_measurements()).map(x => timingInMs(() => {
-    Thread.sleep(1000) // Do everything here from train and test
-    println(train.map(x => abs(x.rating - meanGlobal)).sum)
-    42        // Output answer as last value
-  }))
-  val timings = measurements.map(t => t._2) // Retrieve the timing measurements
+  // Get timing for each of the 4 methods
+  var timings = scala.collection.mutable.Map.empty[String, Seq[Double]]
+  for (predictor: String <- Array("Baseline", "Global", "User", "Item")) {
+    val measurements = (1 to conf.num_measurements()).map(x => timingInMs(() => {
+      predictor match {
+        case "Baseline" => solvers.getMAE(solvers.getBaselinePredictions, test)
+        case "Global" => solvers.getMAE(solvers.getGlobalPredictions, test)
+        case "User" => solvers.getMAE(solvers.getUserAvgPredictions, test)
+        case "Item" => solvers.getMAE(solvers.getItemAvgPredictions, test)
+      }
+    }))
+    timings(predictor) = measurements.map(t => t._2)
+  }
 
   // Save answers as JSON
-  def printToFile(content: String, 
+  def printToFile(content: String,
                   location: String = "./answers.json") =
-    Some(new java.io.PrintWriter(location)).foreach{
-      f => try{
-        f.write(content)
-      } finally{ f.close }
-  }
+    Some(new java.io.PrintWriter(location)).foreach {
+      f =>
+        try {
+          f.write(content)
+        } finally {
+          f.close
+        }
+    }
 
   conf.json.toOption match {
     case None => ;
@@ -81,35 +75,34 @@ object Baseline extends App {
           "3.Measurements" -> ujson.Num(conf.num_measurements())
         ),
         "B.1" -> ujson.Obj(
-          "1.GlobalAvg" -> meanGlobal, // Datatype of answer: Double
-          "2.User1Avg" -> user1Avg,  // Datatype of answer: Double
-          "3.Item1Avg" -> item1Avg,   // Datatype of answer: Double
-          "4.Item1AvgDev" -> item1AvgDev, // Datatype of answer: Double
-          "6.scaleItem1AvgDev" -> item1AvgDevScale, // Datatype of answer: Double
-          "5.PredUser1Item1" -> predUser1Item1 // Datatype of answer: Double
+          "1.GlobalAvg" -> solvers.getGlobalAvg, // Datatype of answer: Double
+          "2.User1Avg" -> solvers.getUserAvg(1), // Datatype of answer: Double
+          "3.Item1Avg" -> solvers.getItemAvg(1), // Datatype of answer: Double
+          "4.Item1AvgDev" -> solvers.getItemAvgDev(1), // Datatype of answer: Double
+          "5.PredUser1Item1" -> solvers.getPredUserItem(1, 1) // Datatype of answer: Double
         ),
         "B.2" -> ujson.Obj(
-          "1.GlobalAvgMAE" -> ujson.Num(0.0), // Datatype of answer: Double
-          "2.UserAvgMAE" -> ujson.Num(0.0),  // Datatype of answer: Double
-          "3.ItemAvgMAE" -> ujson.Num(0.0),   // Datatype of answer: Double
-          "4.BaselineMAE" -> ujson.Num(0.0)   // Datatype of answer: Double
+          "1.GlobalAvgMAE" -> solvers.getMAE(solvers.getGlobalPredictions, test), // Datatype of answer: Double
+          "2.UserAvgMAE" -> solvers.getMAE(solvers.getUserAvgPredictions, test), // Datatype of answer: Double
+          "3.ItemAvgMAE" -> solvers.getMAE(solvers.getItemAvgPredictions, test), // Datatype of answer: Double
+          "4.BaselineMAE" -> solvers.getMAE(solvers.getBaselinePredictions, test) // Datatype of answer: Double
         ),
         "B.3" -> ujson.Obj(
           "1.GlobalAvg" -> ujson.Obj(
-            "average (ms)" -> ujson.Num(mean(timings)), // Datatype of answer: Double
-            "stddev (ms)" -> ujson.Num(std(timings)) // Datatype of answer: Double
+            "average (ms)" -> ujson.Num(mean(timings("Global"))), // Datatype of answer: Double
+            "stddev (ms)" -> ujson.Num(std(timings("Global"))) // Datatype of answer: Double
           ),
           "2.UserAvg" -> ujson.Obj(
-            "average (ms)" -> ujson.Num(mean(timings)), // Datatype of answer: Double
-            "stddev (ms)" -> ujson.Num(std(timings)) // Datatype of answer: Double
+            "average (ms)" -> ujson.Num(mean(timings("User"))), // Datatype of answer: Double
+            "stddev (ms)" -> ujson.Num(std(timings("User"))) // Datatype of answer: Double
           ),
           "3.ItemAvg" -> ujson.Obj(
-            "average (ms)" -> ujson.Num(mean(timings)), // Datatype of answer: Double
-            "stddev (ms)" -> ujson.Num(std(timings)) // Datatype of answer: Double
+            "average (ms)" -> ujson.Num(mean(timings("Item"))), // Datatype of answer: Double
+            "stddev (ms)" -> ujson.Num(std(timings("Item"))) // Datatype of answer: Double
           ),
           "4.Baseline" -> ujson.Obj(
-            "average (ms)" -> ujson.Num(mean(timings)), // Datatype of answer: Double
-            "stddev (ms)" -> ujson.Num(std(timings)) // Datatype of answer: Double
+            "average (ms)" -> ujson.Num(mean(timings("Baseline"))), // Datatype of answer: Double
+            "stddev (ms)" -> ujson.Num(std(timings("Baseline"))) // Datatype of answer: Double
           )
         )
       )
--- cs449-Template-M1-master/src/test/scala/shared/helpers.scala
+++
@@ -1,9 +0,0 @@
-package tests.shared
-
-package object helpers {
-
-  def within(actual :Double, expected :Double, interval :Double) : Boolean = {
-    return actual >= (expected - interval) && actual <= (expected + interval)
-  }
-
-}
